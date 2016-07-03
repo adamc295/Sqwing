4bfb034512bdd4ca72a4269dd306d3d0a8750704
adamc295 <mstowe584923@gmail.com>
2016-07-03 13:01:43 +0100

    Creation

---
+++ Panel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+
+public class Panel extends JPanel implements Runnable {
+    public Panel {
+        System.out.println("WIP");
+    }
+}
